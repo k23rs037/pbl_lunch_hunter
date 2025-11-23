8df5db54015b1a6d5c51e2a083d68d1b2d3bda2a
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-11-24 08:57:01 +0900

    Update

---
+++ src/user_check.php
@@ -0,0 +1,12 @@
+<?php
+$user_id = $_POST['user_id'];
+$user_password=$_POST['user_password'];
+if($user_id = "kyusan" && $user_password = "pbl2025"){ //動作チェック用
+    $_SESSION['user_id'] = $user_id;
+    $_SESSION['user_paswword'] = $user_paswword;
+    $_SESSION['usertype_id'] = 1;
+    header('Location:index.php');
+} else {
+    header('Location:?do=user_login');
+}
+?>
---
+++ src/user_logout.php
@@ -0,0 +1,4 @@
+<?php
+unset($_SESSION);
+session_destroy();
+header('Location:?do=user_login');
