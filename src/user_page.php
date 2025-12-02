dec7e7b6f562ef24acb84172c485f2c0bc53477f
k23RS064 <k23rs064@st.kyusan-u.ac.jp>
2025-12-02 10:41:30 +0900

    Update user_page.php

---
+++ src/user_page.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="MY PAGE.css">
+    <title>MY PAGE</title>
+</head>
+<body>
+    <div class="main">
+        <h1>マイページ</h1>
+        <!--ユーザ情報編集btn-->
+        <a class="btn" href="">ユーザ情報編集</a>
+    </div>
+    <!--アカウント情報-->
+    <div class="info">
+        <div>
+            <div class="item1">社員ID</div>
+            <input type="text" placeholder="社員IDを入力">
+        </div>
+        <div>
+            <div class="item1">氏名</div>
+            <input type="text" placeholder="姓">
+            <input type="text" placeholder="名">
+        </div>
+    </div>
+    <div class="info1">
+        <div>
+            <div class="item1">アカウント名</div>
+            <input type="text" placeholder="アカウント名を入力">
+        </div>
+        <div>
+            <div class="item1">フリガナ</div>
+            <input type="text" placeholder="セイ">
+            <input type="text" placeholder="メイ">
+        </div>
+    </div>
+    <!--投稿店舗-->
+    <div class="shop">
+        <div class="item">
+            <div class="shopi">
+                <h4>店舗名</h4>
+                <div>★★★★4.2</div>
+                <div>ジャンル</div>
+            </div>
+            <div class="phot">
+                <img class="img" src="" alt="未登録">
+            </div>
+        </div>
+        <div class="item">
+            <div class="shopi">
+                <h4>店舗名</h4>
+                <div>★★★★4.2</div>
+                <div>ジャンル</div>
+            </div>
+            <div class="phot">
+                <img src="" alt="未登録">
+            </div>
+        </div>
+        <div class="item">
+            <div class="shopi">
+                <h4>店舗名</h4>
+                <div>★★★★4.2</div>
+                <div>ジャンル</div>
+            </div>
+            <div class="phot">
+                <img src="" alt="未登録">
+            </div>
+        </div>
+        <div class="item">
+            <div class="shopi">
+                <h4>店舗名</h4>
+                <div>★★★★4.2</div>
+                <div>ジャンル</div>
+            </div>
+            <div class="phot">
+                <img src="" alt="未登録">
+            </div>
+        </div>
+        <div class="item">
+            <div class="shopi">
+                <h4>店舗名</h4>
+                <div>★★★★4.2</div>
+                <div>ジャンル</div>
+            </div>
+            <div class="phot">
+                <img src="" alt="未登録">
+            </div>
+        </div>
+        <div class="item">
+            <div class="shopi">
+                <h4>店舗名</h4>
+                <div>★★★★4.2</div>
+                <div>ジャンル</div>
+            </div>
+            <div class="phot">
+                <img src="" alt="未登録">
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
