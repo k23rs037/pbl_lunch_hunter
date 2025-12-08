445921d3c10a59d886e5fbacbb0dc7ed4be15152
Kouei Okabe <k23rs037@st.kyusan-u.ac.jp>
2025-12-08 13:40:37 +0900

    Update user_list.php

--- src/user_list.php
+++ src/user_list.php
@@ -4,9 +4,40 @@ $accounts = [
     ['name' => '斎藤巧', 'kana' => 'サイトウ コウ', 'id' => 'u0001', 'account' => 'タックン'],
     ['name' => '田中花子', 'kana' => 'タナカ ハナコ', 'id' => '390002', 'account' => 'ハナちゃん'],
     ['name' => '田村時丸', 'kana' => 'タムラ トキマル', 'id' => '390003', 'account' => '坂上田村丸'],
+    ['name' => '鈴木一郎', 'kana' => 'スズキ イチロウ', 'id' => 'u0004', 'account' => 'イチロー'],
+    ['name' => '山田太郎', 'kana' => 'ヤマダ タロウ', 'id' => '550005', 'account' => 'タロウ'],
+    ['name' => '佐藤二郎', 'kana' => 'サトウ ジロウ', 'id' => '660006', 'account' => 'ジロー'],
+    ['name' => '中島美香', 'kana' => 'ナカジマ ミカ', 'id' => 'u0007', 'account' => 'ミカちゃん'],
+    ['name' => '小林優子', 'kana' => 'コバヤシ ユウコ', 'id' => '880008', 'account' => '優子'],
     // ...最大10件表示
 ];
+
+// --- 検索処理 ---
+$search_key = $_GET['search'] ?? '';
+$filtered_accounts = $accounts;
+
+if (!empty($search_key)) {
+    $key_lower = mb_strtolower($search_key, 'UTF-8');
+    
+    $filtered_accounts = array_filter($accounts, function($user) use ($key_lower) {
+        $id_match = (
+            isset($user['id']) && 
+            mb_stripos(mb_strtolower($user['id'], 'UTF-8'), $key_lower) !== false
+        );
+        $account_match = (
+            isset($user['account']) && 
+            mb_stripos(mb_strtolower($user['account'], 'UTF-8'), $key_lower) !== false
+        );
+        return $id_match || $account_match;
+    });
+    
+    $filtered_accounts = array_values($filtered_accounts);
+}
+
+// 実際にテーブルで表示するデータをフィルタ後の配列に設定
+$accounts_to_display = $filtered_accounts;
 ?>
+
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -30,7 +61,7 @@ $accounts = [
 <div class="search-box">
     <form method="get">
         <label for="search">IDまたはアカウント名：</label>
-        <input type="text" id="search" name="search">
+        <input type="text" id="search" name="search" value="<?= htmlspecialchars($search_key) ?>">
         <button type="submit">検索</button>
     </form>
 </div>
@@ -51,14 +82,20 @@ $accounts = [
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($accounts as $user): ?>
-        <tr>
-            <td><?= htmlspecialchars($user['name']) ?>（<?= htmlspecialchars($user['kana']) ?>）</td>
-            <td><?= htmlspecialchars($user['id']) ?></td>
-            <td><?= htmlspecialchars($user['account']) ?></td>
-            <td><a href="?do=user_edit.php?id=<?= urlencode($user['id']) ?>">編集</a></td>
-        </tr>
-        <?php endforeach; ?>
+        <?php if (!empty($accounts_to_display)): ?>
+            <?php foreach ($accounts_to_display as $user): ?>
+            <tr>
+                <td><?= htmlspecialchars($user['name']) ?>（<?= htmlspecialchars($user['kana']) ?>）</td>
+                <td><?= htmlspecialchars($user['id']) ?></td>
+                <td><?= htmlspecialchars($user['account']) ?></td>
+                <td><a href="?do=user_edit&id=<?= urlencode($user['id']) ?>">編集</a></td>
+            </tr>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <tr>
+                <td colspan="4">該当するアカウントは見つかりませんでした。</td>
+            </tr>
+        <?php endif; ?>
     </tbody>
 </table>
 
